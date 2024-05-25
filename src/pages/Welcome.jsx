68ee920493f3aec98c24f173d55df66138b3afdf
braverokmc79 <braverokmc79@gmail.com>
2024-05-25 13:36:20 +0900

    506.스크롤 기반 애니메이션

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,23 +2,38 @@ import { Link } from "react-router-dom";
 import cityImg from "../assets/city.jpg";
 import heroImg from "../assets/hero.png";
 import Footer from "../components/Footer";
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function WelcomePage() {
+
+  const {scrollY} =useScroll();
+
+  const opacityCity = useTransform(scrollY, [0, 200, 300, 500], [1, 0.5, 0.5, 0]);
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+
+  const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+  const yHero = useTransform(scrollY, [0, 200], [0, -150]);
+
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.5]);
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div     id="welcome-header-content" style={{ scale: scaleText, y: yText }} >
           <h1>도전할 준비가 되셨나요?</h1>
           <Link id="cta-link" to="/challenges">
             시작
           </Link>
-        </div>
-        <img
+        </motion.div>
+        <motion.img          
+          style={{opacity:opacityCity , y:yCity}}
           src={cityImg}
           alt="햇빛이 닿은 도시의 스카이라인"
           id="city-image"
         />
-        <img src={heroImg} alt="망토를 쓴 슈퍼히어로" id="hero-image" />
+        <motion.img src={heroImg} alt="망토를 쓴 슈퍼히어로" id="hero-image" style={{opacity:opacityHero , y:yHero}} />
       </header>
       <main id="welcome-content">
         <section>
